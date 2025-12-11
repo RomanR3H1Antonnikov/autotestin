0bb980f02fff63488a0a2525aa3be49ddff24457
Роман <roman.v.antonnikov@gmail.com>
2025-12-11 14:59:26 +0300

    Реализовано задание "ДЗ: Открытие браузера с помощью Selenium"

--- selenium_practice.py
+++ selenium_practice.py
@@ -1,4 +1,4 @@
-from requests import options
+import time
 from selenium import webdriver
 from selenium.webdriver.edge.service import Service as EdgeService
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
@@ -10,3 +10,5 @@ driver = webdriver.Edge(options=options, service=EdgeService(EdgeChromiumDriverM
 base_url = "https://www.saucedemo.com/" #Тестовый сайт для Selenium
 driver.get(base_url)
 driver.set_window_size(1920, 1080) # Настойка разрешения монитора
+time.sleep(10)
+driver.close()
